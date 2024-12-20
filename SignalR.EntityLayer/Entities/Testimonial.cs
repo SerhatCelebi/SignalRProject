ff9c70f83b411c563f6ddfacd9ed5a98386862b3
SerhatCelebi <serhat6701@gmail.com>
2024-12-20 23:07:57 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Testimonial.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public int ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
